0d0cb821db32874c4c6872672701bb673717c4fe
allen-yyy <59130189+allen-yyy@users.noreply.github.com>
2021-01-30 19:24:57 +0800

    1

--- haribote/hd.c
+++ haribote/hd.c
@@ -2,68 +2,83 @@
 #include "hd.h"
 
 
-void print_identify_info(u16* hdinfo)
+//Several low level routines to test specified flags in hard disk driver registers.
+static BOOL WaitForRdy(WORD wPort,DWORD dwMillionSecond)
 {
-	int i, k;
-	char s[64];
-	struct iden_info_ascii {
-		int idx;
-		int len;
-		char * desc;
-	} iinfo[] = {{10, 20, "HD SN"}, /* Serial number in ASCII */
-		     {27, 40, "HD Model"} /* Model number in ASCII */ };
+	BYTE Status;
+	BOOL bResult = FALSE;
+	int cnt = 65536;
 
-	for (k = 0; k < sizeof(iinfo)/sizeof(iinfo[0]); k++) {
-		char * p = (char*)&hdinfo[iinfo[k].idx];
-		for (i = 0; i < iinfo[k].len/2; i++) {
-			s[i*2+1] = *p++;
-			s[i*2] = *p++;
+	while(TRUE)
+	{
+		Status = io_in8(wPort);
+		if(DRIVER_READY(Status))
+		{
+			bResult = TRUE;
+			break;
 		}
-		s[i*2] = 0;
-		printl("%s: %s\n", iinfo[k].desc, s);
+		cnt --;
+		if(0 == cnt)
+		{
+			break;
+		}
 	}
-
-	int capabilities = hdinfo[49];
-	printl("LBA supported: %s\n",
-	       (capabilities & 0x0200) ? "Yes" : "No");
-
-	int cmd_set_supported = hdinfo[83];
-	printl("LBA48 supported: %s\n",
-	       (cmd_set_supported & 0x0400) ? "Yes" : "No");
-
-	int sectors = ((int)hdinfo[61] << 16) + hdinfo[60];
-	printl("HD size: %dMB\n", sectors * 512 / 1000000);
+	return TRUE;
 }
-void hd_cmd_out(struct hd_cmd* cmd)
-{
-	/**
-	 * For all commands, the host must first check if BSY=1,
-	 * and should proceed no further unless and until BSY=0
-	 */
-	//if (!(in_byte(REG_STATUS) & mask)
-	//	panic("hd error.");
 
-	/* Activate the Interrupt Enable (nIEN) bit */
-	out_byte(REG_DEV_CTRL, 0);
-	/* Load required parameters in the Command Block Registers */
-	out_byte(REG_FEATURES, cmd->features);
-	out_byte(REG_NSECTOR,  cmd->count);
-	out_byte(REG_LBA_LOW,  cmd->lba_low);
-	out_byte(REG_LBA_MID,  cmd->lba_mid);
-	out_byte(REG_LBA_HIGH, cmd->lba_high);
-	out_byte(REG_DEVICE,   cmd->device);
-	/* Write the command code to the Command Register */
-	out_byte(REG_CMD,     cmd->command);
+static BOOL WaitForBsy(WORD wPort,DWORD dwMillionSecond)
+{
+	BYTE Status;
+	int cnt = 65536;
+	BOOL bResult = FALSE;
+	
+	while(TRUE)
+	{
+		Status = io_in8(wPort);  //Read status register.
+		if(CONTROLLER_READY(Status))
+		{
+			bResult = TRUE;
+			break;
+		}
+		cnt --;
+		if(0 == cnt)
+		{
+			break;
+		}
+	}
+	return bResult;
 }
-void hd_identify(int drive)
-{
-	struct hd_cmd cmd;
-	cmd.device  = MAKE_DEVICE_REG(0, drive, 0);
-	cmd.command = ATA_IDENTIFY;
-	hd_cmd_out(&cmd);
-	//interrupt_wait();
-	port_read(REG_DATA, hdbuf, SECTOR_SIZE);
 
-	print_identify_info((u16*)hdbuf);
+static BOOL WaitForDrq(WORD wPort,DWORD dwMillionSecond)
+{
+	BYTE Status = 0;
+	BOOL bResult = FALSE;
+	int cnt = 65536;
+
+	while(TRUE)
+	{
+		Status = io_in8(wPort);
+		if(Status & 8)
+		{
+			bResult = TRUE;
+			break;
+		}
+		cnt --;
+		if(0 == cnt)
+		{
+			break;
+		}
+	}
+	return bResult;
+}
+
+static BOOL CmdSucc(WORD wPort)
+{
+	BYTE status = io_in8(wPort);
+	if(COMMAND_SUCC(status))
+	{
+		return TRUE;
+	}
+	return FALSE;
 }
 
